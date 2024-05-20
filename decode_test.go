928a06d27076134dbab3d11fe31324e819180664
syauqidamario <syauqi.djohan@student.umn.ac.id>
2024-05-20 17:52:08 +0700

    Go-Lang - Decode JSON

---
+++ decode_test.go
@@ -0,0 +1,24 @@
+package golang_json
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestDecodeJSON(t *testing.T) {
+	jsonString := `{"FirstName":"Syauqi","MiddleName":"Damario","LastName":"Djohan","Age":24,"Married":false}`
+	jsonBytes := []byte(jsonString)
+
+	customer := &Customer{}
+
+	err := json.Unmarshal(jsonBytes, customer)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(customer)
+	fmt.Println(customer.FirstName)
+	fmt.Println(customer.MiddleName)
+	fmt.Println(customer.LastName)
+}
