1a2677506173779dfd2df56f84ef8af1465225af
syauqidamario <syauqi.djohan@student.umn.ac.id>
2024-05-20 17:43:49 +0700

    Go-Lang JSON - JSON Object & Encode

---
+++ json_object_test.go
@@ -0,0 +1,38 @@
+package golang_json
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+//struct adalah representasi objek JSON
+//atribut object adalah atribut struct
+
+type Customer struct {
+	FirstName string
+	MiddleName string
+	LastName string
+	Age int
+	Married bool
+}
+
+// type Address struct {
+// 	Street     string
+// 	Country    string
+// 	PostalCode string
+// }
+
+func TestJSONObject(t *testing.T) {
+	customer := Customer{
+		FirstName: "Syauqi",
+		MiddleName: "Damario",
+		LastName: "Djohan",
+		Age: 24,
+		Married: false,
+	}
+
+	bytes, _ := json.Marshal(customer)
+
+	fmt.Println(string(bytes))
+}
